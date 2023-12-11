038fb4170d81cb82751f8d42b13dafbea419c466
japheth-alt163 <japhethbaraka4@gmail.com>
2023-12-11 06:03:23 -0800

    done

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,2 +1,5 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/Holberton/).join
+# The above line specifies that the script should be interpreted by the Ruby interpreter.
+
+# Output the substring that matches the regular expression /hbt{2,5}n/
+puts ARGV[0].scan(/hbt{2,5}n/).join
--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,2 +1,5 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/Holberton/).join
+# The above line specifies that the script should be interpreted by the Ruby interpreter.
+
+# Output the substring that matches the regular expression /hb{0,1}tn/
+puts ARGV[0].scan(/hb{0,1}tn/).join
