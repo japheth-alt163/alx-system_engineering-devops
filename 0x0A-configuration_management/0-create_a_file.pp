bd53cf7806b35130552bf96827af6c24c4bde494
japheth-alt163 <japhethbaraka4@gmail.com>
2024-01-19 01:17:28 -0800

    task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Puppet Manifest for Creating a File in /tmp
+
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
